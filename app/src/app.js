035a488fa1a8af622a43dccf1a5dcaeb2883acc5
Tommy Jiang <tianyangj@gmail.com>
2015-07-17 15:18:54 -0700

    style changes

--- app/src/app.js
+++ app/src/app.js
@@ -1,51 +1,104 @@
 angular
 	.module('GameFly', ['ngMaterial'])
-	.config(function ($mdThemingProvider, $mdIconProvider) {
+	.config(function ($mdThemingProvider, $mdIconProvider, $sceProvider) {
+
+		$sceProvider.enabled(false);
 
 		$mdIconProvider
-			.defaultIconSet("./assets/svg/avatars.svg", 128)
 			.icon("menu", "./assets/svg/menu.svg", 24)
-			.icon("share", "./assets/svg/share.svg", 24)
-			.icon("google_plus", "./assets/svg/google_plus.svg", 512)
-			.icon("hangouts", "./assets/svg/hangouts.svg", 512)
-			.icon("twitter", "./assets/svg/twitter.svg", 512)
-			.icon("phone", "./assets/svg/phone.svg", 512);
+			.icon("play", "./assets/svg/ic_play.svg", 36)
+			.icon("pause", "./assets/svg/ic_pause.svg", 36);
 
 		$mdThemingProvider.theme('default')
 			.primaryPalette('deep-orange')
-			.accentPalette('green');
+			.accentPalette('indigo');
 
 	})
-	.controller('AppController', function () {
-		this.products = [{
-			image: 'http://placehold.it/400x300',
-			title: 'Batman: Arkham Knight',
-			video: 'https://cdn1.gameflycdn.com/merch/Video/480p/product-5007751-4.mp4',
-			poster: 'https://cms.gameflycdn.com/merchandising/live/Design/Product/5006814/rendered.clean-video.960w.jpg'
+	.controller('AppController', function ($scope) {
+
+		$scope.products1 = [{
+			image: '/assets/image/s1.png',
+			title: 'Sleeping Dogs: Definitive Edition',
+			video: 'https://cdn1.gameflycdn.com/merch/Video/1080p/product-5008749.mp4'
 		}, {
-				image: 'http://placehold.it/400x300',
+				image: '/assets/image/s2.png',
+				title: 'WRC 4: FIA World Rally Championship',
+				video: 'https://cdn1.gameflycdn.com/merch/Video/1080p/product-5008929.mp4'
+			}, {
+				image: '/assets/image/s3.png',
+				title: 'Batman: Arkham Knight',
+				video: 'https://cdn1.gameflycdn.com/merch/Video/480p/product-5007751-4.mp4'
+			}, {
+				image: '/assets/image/s1.png',
 				title: 'Witcher: Wild Hunt',
-				video: 'https://cdn1.gameflycdn.com/merch/Video/480p/product-5006815-2.mp4',
-				poster: 'https://cms.gameflycdn.com/merchandising/live/Design/Product/5006815/rendered.clean-video.960w.jpg'
-			}, {
-				image: 'http://placehold.it/400x300',
-				title: 'Batman: Arkham Knight',
-				video: 'https://cdn1.gameflycdn.com/merch/Video/480p/product-5007751-4.mp4',
-				poster: 'https://cms.gameflycdn.com/merchandising/live/Design/Product/5006814/rendered.clean-video.960w.jpg'
-			}, {
-				image: 'http://placehold.it/400x300',
-				title: 'Batman: Arkham Knight',
-				video: 'https://cdn1.gameflycdn.com/merch/Video/480p/product-5007751-4.mp4',
-				poster: 'https://cms.gameflycdn.com/merchandising/live/Design/Product/5006814/rendered.clean-video.960w.jpg'
-			}, {
-				image: 'http://placehold.it/400x300',
-				title: 'Batman: Arkham Knight',
-				video: 'https://cdn1.gameflycdn.com/merch/Video/480p/product-5007751-4.mp4',
-				poster: 'https://cms.gameflycdn.com/merchandising/live/Design/Product/5006814/rendered.clean-video.960w.jpg'
-			}, {
-				image: 'http://placehold.it/400x300',
-				title: 'Batman: Arkham Knight',
-				video: 'https://cdn1.gameflycdn.com/merch/Video/480p/product-5007751-4.mp4',
-				poster: 'https://cms.gameflycdn.com/merchandising/live/Design/Product/5006814/rendered.clean-video.960w.jpg'
+				video: 'https://cdn1.gameflycdn.com/merch/Video/480p/product-5006815-2.mp4'
 			}];
-	});
+
+		$scope.products2 = [{
+			image: '/assets/image/s2.png',
+			title: 'Batman: Arkham Knight'
+		}, {
+				image: '/assets/image/s3.png',
+				title: 'Batman: Arkham Knight'
+			}, {
+				image: '/assets/image/s1.png',
+				title: 'Batman: Arkham Knight'
+			}, {
+				image: '/assets/image/s2.png',
+				title: 'Batman: Arkham Knight'
+			}, {
+				image: '/assets/image/s3.png',
+				title: 'Batman: Arkham Knight'
+			}, {
+				image: '/assets/image/s1.png',
+				title: 'Batman: Arkham Knight'
+			}, {
+				image: '/assets/image/s2.png',
+				title: 'Batman: Arkham Knight'
+			}];
+
+		$scope.play = function (index) {
+			$scope.activeIndex = index;
+		};
+	}).directive('gfSplashVideo', ['$timeout', function ($timeout) {
+
+		return {
+			restrict: 'E',
+			template: '<h1 style="position:absolute;left:0;right:0;background-color:rgba(0,0,0,0.5);color:#fff;padding:30px;margin:0;">{{activeVideo.title}}</h1><video muted ng-src="{{activeVideo.video}}" style="width:100%;"></video>',
+			scope: {
+				items: '=',
+				activeIndex: '='
+			},
+			link: function (scope, el) {
+
+				scope.$watch('activeIndex', function (newIndex) {
+					if (!angular.isUndefined(newIndex)) {
+						// set active video item style
+						var $items = angular.element('.my-game');
+						var $activeItem = angular.element($items[newIndex]);
+						$items.css('opacity', '0.5');
+						$activeItem.css('opacity', '1');
+						// play video
+						scope.activeVideo = scope.items[newIndex];
+						$timeout(function () {
+							el.find('video')[0].play();
+						});
+					}
+				});
+
+				if (angular.isUndefined(scope.activeIndex) && scope.items.length) {
+					scope.activeIndex = 0;
+				}
+
+				el.find('video').on('ended', function () {
+					scope.$apply(function () {
+						if (scope.activeIndex === scope.items.length - 1) {
+							scope.activeIndex = 0;
+						} else {
+							scope.activeIndex++;
+						}
+					});
+				});
+			}
+		};
+	}]);
